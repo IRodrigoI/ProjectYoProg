076515cd5d0214f96e3566ebb0f6c92b668021ea
IRodrigoI <99357213+IRodrigoI@users.noreply.github.com>
2022-06-29 15:26:26 -0300

    Arreglos

--- ProjectYoProg/src/app/app.module.ts
+++ ProjectYoProg/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArPComponent } from './components/logo-ar-p/logo-ar-p.component';
-import { SocNetComponent } from './components/soc-net/soc-net.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ExperienceComponent } from './components/experience/experience.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArPComponent,
-    SocNetComponent,
     BannerComponent,
     AboutMeComponent,
     ExperienceComponent,
